85b0a05ea74133de21504488d0fdc9726b43c619
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-06-16 20:18:47 -0300

    cabecalho, rodape, banner

---
+++ src/pages/Inicio/components/Rodape/index.js
@@ -0,0 +1,23 @@
+import styles from './Rodape.module.css'
+import { IoLogoInstagram, IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
+
+function Rodape() {
+    return (
+        <footer className={styles.rodape}>
+            <h2 className={styles.texto}>Desenvolvido por Sidney Rodrigues Magalhães</h2>
+            <div class={styles.redes_sociais}>
+                <a href="https://github.com/sidneygyne" target="_blank" rel="noopener noreferrer" >
+                    <IoLogoGithub className={styles.icone} alt="imagem github" />
+                </a>
+                <a href="https://www.linkedin.com/in/sidneygyn" target="_blank" rel="noopener noreferrer" >
+                    <IoLogoLinkedin className={styles.icone} alt="imagem linkedin" />
+                </a>
+                <a href="https://instragram.com/sidneygyn" target="_blank" rel="noopener noreferrer" >
+                    <IoLogoInstagram className={styles.icone} alt="imagem instragram" />
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Rodape
---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,17 @@
+
+import Rodape from "./components/Rodape";
+import Cabecalho from "./components/Cabecalho";
+import Banner from "./components/Banner";
+
+function Inicio() {
+    return (
+        <>
+            <Cabecalho />
+            <Banner imagem="Home"/>
+            <Rodape/>
+        </>
+
+    )
+}
+
+export default Inicio
