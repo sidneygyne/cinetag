820a9bbef93afdfa081509bbc968275fd383f75d
Sidney Rodrigues <sidneyr_gyn@hotmail.com>
2024-06-18 08:46:23 -0300

    containers

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,13 +1,26 @@
-
 import Rodape from "./components/Rodape";
 import Cabecalho from "./components/Cabecalho";
 import Banner from "./components/Banner";
+import Titulo from "./components/Titulo";
+import Card from "./components/Card";
+import videos from 'json/db.json';
+import styles from './Inicio.module.css';
 
 function Inicio() {
     return (
         <>
             <Cabecalho />
             <Banner imagem="Home"/>
+            <Titulo children="">
+                <h1>Um lugar para guardar seus vídeos e filmes!</h1>
+            </Titulo>
+                <section className={styles.container}>
+                {videos.map((video) => {
+                return <Card {...video} key={video.id} />
+                })}
+            
+            </section>
+            
             <Rodape/>
         </>
 
